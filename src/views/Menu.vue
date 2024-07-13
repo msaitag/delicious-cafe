d2c3924aa019431d39c07154362062c227ad6851
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-13 11:06:22 +0200

    Update Menu.vue

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -59,21 +59,27 @@ import Footer from "../components/Footer.vue"
 export default {
   data(){
     return{
-      breakfasts: [],
-      lunchs:[],
-      dinners:[]
+      breakfasts: [{"title": "cream cheese pancakes","id": 1, "ingredients": "cream cheese, eggs, sugar, salt, butter, mixed fruits", "image": "breakfast1.jpg"},
+        {"title": "greek avocado toast", "id": 2, "ingredients": "Toasted Bread, Avocado, Feta Cheese, Tomato, Olives", "image": "breakfast2.jpg"},
+        {"title": "tropical oatmeal", "id": 3, "ingredients": "Oat, Coconut Milk, Mango, Coconut Flakes, Mixed Berries", "image": "breakfast3.jpg"}],
+      lunchs:[{"title": "thai tom kha soup","id": 1, "ingredients": "Chicken, Coconut Milk, Galangal, Lemongrass, Thai Chili, Kaffir Lime Mushrooms, Fish sauce, Lime Juice", "image": "lunch1.jpg"},
+        {"title": "roasted lamb","id": 2, "ingredients": "Boneless Lamb Leg, Garlic, Onion, Spices, Rosemary", "image": "lunch2.jpg"},
+        {"title": "tomato basil soup","id": 3, "ingredients": "Tomatos, Basil, Spices, Salt", "image": "lunch3.jpg"}],
+      dinners:[{"title": "asian chicken wrap","id": 1, "ingredients": "Sliced Chicken, Broccoli Slaw, Mixed Greens, Wrap, Sesame Dressing", "image": "dinner1.jpg"},
+        {"title": "char grilled prawns","id": 2, "ingredients": "King Prawns, Garlic, Herbs, Olive Oil, Lemon", "image": "dinner2.jpg"},
+        {"title": "garlic mushrooms","id": 3, "ingredients": "Mushrooms, Tomato, Garlic, Herbs, Olive Oil", "image": "dinner3.jpg"}]
     }
   },
-  async mounted(){
-     let breakfasts = await axios.get("https://delicious-cafe.onrender.com/breakfast")
-     this.breakfasts = breakfasts.data
+ // async mounted(){
+   //  let breakfasts = await axios.get("https://delicious-cafe.onrender.com/breakfast")
+    // this.breakfasts = breakfasts.data
 
-     let lunchs = await axios.get("https://delicious-cafe.onrender.com/lunch")
-     this.lunchs = lunchs.data
+    // let lunchs = await axios.get("https://delicious-cafe.onrender.com/lunch")
+    // this.lunchs = lunchs.data
 
-     let dinners = await axios.get("https://delicious-cafe.onrender.com/dinner")
-     this.dinners = dinners.data
-  },
+    // let dinners = await axios.get("https://delicious-cafe.onrender.com/dinner")
+    // this.dinners = dinners.data
+ // },
   components: {
     Footer
   }
